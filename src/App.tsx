47dd1c04281704407a705fa876eb5653840483be
Nathan Klingensmith <njklingensmith@wpi.edu>
2022-02-01 20:35:38 -0500

    Add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import './App.scss';
+import { Outlet } from 'react-router-dom';
 
 function App() {
-  return <div className='app'></div>;
+  return (
+    <div>
+      Hello world!
+      <Outlet />
+    </div>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './routes/home';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+render(
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />}>
+        <Route path='home' element={<Home />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
